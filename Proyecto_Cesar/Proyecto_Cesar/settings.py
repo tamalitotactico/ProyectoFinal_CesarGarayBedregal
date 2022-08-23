5f774cde7c853ee16da01d49d3f1c869542218a2
César Alejandro Garay Bedregal <cgarayb@unsa.edu.pe>
2022-08-23 11:18:09 -0500

    Fixing procfile error 5

--- Proyecto_Cesar/Proyecto_Cesar/settings.py
+++ Proyecto_Cesar/Proyecto_Cesar/settings.py
@@ -127,7 +127,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_TMP = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+
+os.makedirs(STATIC_TMP, exist_ok=True)
+os.makedirs(STATIC_ROOT, exist_ok=True)
+
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
--- Proyecto_Cesar/Proyecto_Cesar/urls.py
+++ Proyecto_Cesar/Proyecto_Cesar/urls.py
@@ -17,8 +17,8 @@ from re import template
 from django.contrib import admin
 from django.urls import path
 from app1.views import index, contacto, about
-from django.config import settings
-from django.config.urls.static import static
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
